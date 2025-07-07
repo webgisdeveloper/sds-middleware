42ffcd494d4e54495e6c2c5608b6f4e6b76192f1
webgisdeveloper <webgisdeveloper@users.noreply.github.com>
2025-07-07 13:08:30 -0400

    fastapi simple route

--- sds_fastapi/app/main.py
+++ sds_fastapi/app/main.py
@@ -1,6 +1,7 @@
-def main():
-    print("Hello from sds-fastapi!")
+from fastapi import FastAPI
 
+app = FastAPI()
 
-if __name__ == "__main__":
-    main()
+@app.get("/")
+async def read_root():
+    return {"Hello": "World"}
