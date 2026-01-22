5421df8d2182b2beb597405a51e8def41210dbbe
webgisdeveloper <webgisdeveloper@users.noreply.github.com>
2026-01-22 14:01:31 -0500

    restructure - fastAPI only

--- async/README.md
+++
@@ -1,3 +0,0 @@
-# SDS Middleware Containerization
-
-Refer to [SDS Middleware Deployment Guide](Deployment_Guide_README.md) to deploy the middleware based on your specific settings.
--- async/staging_area_clean.sh
+++
@@ -1,8 +0,0 @@
-#/bin/bash
-
-SCRIPT="</path/to/house_keeper.py>"
-DATA_ROOT="</path/to/mounted/staging/area>" # e.g., "/volume-mnt-point/staging"
-TTL_IN_MIN=1440 # time to live in minutes
-WHITE_LIST="</path/to/white_list.csv>" # e.g., path to "ISDPmetadatafilenamesforwhitelist.csv"
-
-python $SCRIPT --dataroot $DATA_ROOT --ttl_in_min $TTL_IN_MIN --white_list $WHITE_LIST
